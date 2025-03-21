e989e8456bb9226ef5440da133da2fc995825685
stive1213 <estifanosamsalu833@gmail.com>
2025-03-21 10:17:31 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import { Container, Row, Col, Button, InputGroup, FormControl, ListGroup } from "react-bootstrap";
+
+
+class App extends component {
+constructor(props) {
+  super(props);
+
+  this.state = {
+    userInput: "",
+    list:[],
+  }
+}
+}
+export default App;
