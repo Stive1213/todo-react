2b686884964ce28066612d79b8f51c4dc7a8ea49
stive1213 <estifanosamsalu833@gmail.com>
2025-03-21 10:39:11 -0700

    `feat: added todo list UI with input field, add button, and list of items`

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,83 @@ editItem = (index) => {
   }
 }
 
+render(){
+  return (
+    <Container>
+    <Row
+        style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            fontWeight: "bolder",
+        }}
+    >
+        TODO LIST
+    </Row>
 
+    <hr />
+    <Row>
+        <Col md={{ span: 5, offset: 4 }}>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="add item . . . "
+                    size="lg"
+                    value={this.state.userInput}
+                    onChange={(item) =>
+                        this.updateInput(item.target.value)
+                    }
+                    aria-label="add something"
+                    aria-describedby="basic-addon2"
+                />
+                <InputGroup>
+                    <Button
+                        variant="dark"
+                        className="mt-2"
+                        onClick={() => this.addItem()}
+                    >
+                        ADD
+                    </Button>
+                </InputGroup>
+            </InputGroup>
+        </Col>
+    </Row>
+    <Row>
+        <Col md={{ span: 5, offset: 4 }}>
+            <ListGroup>
+                {/* map over and print items */}
+                {this.state.list.map((item, index) => {
+                    return (
+                      <div key = {index} > 
+                        <ListGroup.Item
+                            variant="dark"
+                            action
+                            style={{display:"flex",
+                                    justifyContent:'space-between'
+                          }}
+                        >
+                            {item.value}
+                            <span>
+                            <Button style={{marginRight:"10px"}}
+                            variant = "light"
+                            onClick={() => this.deleteItem(item.id)}>
+                              Delete
+                            </Button>
+                            <Button variant = "light"
+                            onClick={() => this.editItem(index)}>
+                              Edit
+                            </Button>
+                            </span>
+                        </ListGroup.Item>
+                      </div>
+                    );
+                })}
+            </ListGroup>
+        </Col>
+    </Row>
+</Container>
+  );
+}
 
 
 }
